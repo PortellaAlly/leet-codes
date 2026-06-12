58e8aa883dc221f81c8d0c02def323c2dceeeca3
Portella <Allysonpalmeirim12@gmail.com>
2026-06-12 17:20:41 -0300

    two sum added

---
+++ twosum/src/main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int j;
+
+        for (j =  nums.length -1; j > 0; j--){
+            for (int i = 0; i < j; i++){
+                int currentSum = nums[i] + nums[j];
+                if (currentSum == target){
+                return new int[]{i, j};
+            }
+        }
+        }
+        return new int[]{ -1, -1};      
+        
+    }
+}
